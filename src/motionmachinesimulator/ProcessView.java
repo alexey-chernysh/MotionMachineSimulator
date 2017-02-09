0ddebf6e0b67c04e674013b6f942e40562c9f542
Alexey Chernysh <alexey.chernysh@gmail.com>
2017-02-09 17:11:27 +0700

    from job

---
+++ src/motionmachinesimulator/ProcessView.java
@@ -0,0 +1,15 @@
+package motionmachinesimulator;
+
+import javax.swing.*;
+
+/**
+ * Created by Sales on 09.02.2017.
+ */
+public class ProcessView {
+    private JPanel mainPanel;
+    private JPanel leftPanel;
+    private JPanel rightPanel;
+    private JButton startButton;
+    private JButton stopButton;
+    private JPanel bottonsPanel;
+}
